009f2f5ed095215b715f1e9f0af3ea35bfcad5e8
szidonia.laszlo <szidonia.laszlo@directtwo.hu>
2019-12-18 15:03:05 +0100

    Initial commit

---
+++ app/src/main/java/d2/hu/demo_v1_kotlin/User.kt
@@ -0,0 +1,5 @@
+package d2.hu.demo_v1_kotlin
+
+data class User(var firstName: String?, var lastName: String?)
+
+//String? ==> User properties (lastname & firstname) can accept null values, each is marked as nullable with this symbol: ?
